21ba27e02590409389533f4378af1cf987821999
mostafa-obiedat <mustafaobiedat3@gmail.com>
2025-01-02 16:08:02 +0300

    update files

--- app.js
+++ app.js
@@ -8,19 +8,26 @@ form1.addEventListener("submit",function(events){
     const username = document.getElementById("username").value;
   const age = document.getElementById("age").value;
   const gender = document.getElementById("gender").value;
-
+  
   const orderChoice = document.querySelector('input[name="orderChoice"]:checked');
   if (!orderChoice) {
     details.textContent = "Please select an order choice.";
     return;
   }
+  const checkbox = document.querySelector('input[name="order"]:checked');
+  if (!checkbox) {
+    details.textContent = "Please select an order size.";
+    return;
+  }
   const selectedOrder = orderChoice.value;
+  const orderSize = checkbox.value;
   details.innerHTML=
  `
     <strong>Username:</strong> ${username} <br>
     <strong>Age:</strong> ${age} <br>
     <strong>Gender:</strong> ${gender} <br>
-    <strong>Order Choice:</strong> ${selectedOrder}`
+    <strong>Order Choice:</strong> ${selectedOrder}<br>
+    <strong>Order Size:</strong> ${orderSize}`
   ;});
 
 
